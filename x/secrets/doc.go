59201454b0807a805f05e2dc25994d5c4df10863
Melanie Matthews <melanie.matthews@cultureamp.com>
2022-07-22 08:44:43 +1000

    Improve the docs, by adding usage instructions and a note on where in the lambda to use this code.

--- x/secrets/doc.go
+++ x/secrets/doc.go
@@ -1,3 +1,14 @@
 // package secrets adds helper methods to fetch secrets from the AWS secret
-// manager.
+// manager for use in AWS lambdas.
+//
+// **Secrets should be fetched during the cold start of the lambda**
+//
+// To create the secret manager client:
+//
+// `secretClient, err := secrets.NewAWSSecretsClient(envConfig.AwsRegion)`
+//
+// then fetch the secret using Get:
+//
+// `secretValue, err = secretClient.Get("/your/secret-name")`
+//
 package secrets
