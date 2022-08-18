2071f6ad910bfb6264c9d22f4c0b9189be510e97
Ellen Welstead-Cloud <ewelstead.cloud@gmail.com>
2022-08-18 14:46:32 +1000

    chore: add example useage in docs

--- x/vaultdecryption/doc.go
+++ x/vaultdecryption/doc.go
@@ -1,3 +1,12 @@
-// package vaultdecryption adds helper methods for decrypting using vault
-
+// Package vaultdecryption adds helper methods for decrypting using vault
+// To Decrypt, you must first create a client and decrypter:
+// settings := client.VaultSettings{
+////				DecrypterRoleArn: <arn here>,
+////				VaultAddr:        <vault address here>,
+////			}
+// client, err :=  client.NewVaultClient(&settings, ctx context.Context)
+//
+// decrypter := NewVaultDecrypter(client, &settings})
+//
+// decryptedSecret, err := decrypter.Decrypt(keyReferences, encryptedData, ctx)
 package vaultdecryption
