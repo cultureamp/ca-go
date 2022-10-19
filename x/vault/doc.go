4e8e34297ad468ac4070d8b096a71b5bbca46502
Melanie Matthews <melanie.matthews@cultureamp.com>
2022-10-19 16:53:37 +1100

    Always use `us-east-1` for STS region. (#82)
    This is because we are using the default STS service in `us-east-1` region

--- x/vault/doc.go
+++ x/vault/doc.go
@@ -21,6 +21,6 @@
 //
 // # If no keys are passed to Encrypt or Decrypt a VaultMissingKeysErr will be thrown
 //
-// The default aws region for generating AWS login data is 'us-east-1' which
-// can be changed by using the env var AWS_REGION
+// The default aws region for generating AWS login data is 'us-east-1'. This is
+// is fixed for all environments.
 package vault
