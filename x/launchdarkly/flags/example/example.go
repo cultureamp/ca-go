9e50d6c29a5975bec49aace6f6ef330c19300743
Simon Lacis <slacis11@gmail.com>
2023-09-05 13:45:39 +1000

    [COSE-1758] remove deprecated context methods (launchdarkly) (#199)
    * remove deprecated methods
    
    * increase lint timeout

--- x/launchdarkly/flags/example/example.go
+++ x/launchdarkly/flags/example/example.go
@@ -8,8 +8,6 @@ import (
 )
 
 func main() {
-	// skip staticcheck linter for this block as there are deprecated methods used, can be removed when legacy code is removed
-	//nolint:staticcheck
 	contexts := map[string]evaluationcontext.Context{
 		"anonymous":  evaluationcontext.NewEvaluationContext(),
 		"user":       evaluationcontext.NewEvaluationContext(evaluationcontext.WithUserID("go-user-1"), evaluationcontext.WithContextRealUserID("go-real-user-1")),
@@ -17,14 +15,6 @@ func main() {
 		"account":    evaluationcontext.NewEvaluationContext(evaluationcontext.WithAccountID("go-account-3")),
 		"subdomain":  evaluationcontext.NewAnonymousContextWithSubdomain("go-account-10", "goSubdomain"),
 		"everything": evaluationcontext.NewEvaluationContext(evaluationcontext.WithUserID("go-user-4"), evaluationcontext.WithContextRealUserID("go-real-user-4"), evaluationcontext.WithAccountID("go-account-4"), evaluationcontext.WithSurveyID("go-survey-4")),
-		// todo: remove these examples once deprecated legacy code is deleted
-		"legacy user": evaluationcontext.NewUser("go-user-5", evaluationcontext.WithRealUserID("go-real-user-5")),
-		"legacy user with account": evaluationcontext.NewUser("go-user-6", evaluationcontext.WithRealUserID("go-real-user-6"),
-			evaluationcontext.WithUserAccountID("go-account-6")),
-		"legacy anonymous user":          evaluationcontext.NewAnonymousUser(""),
-		"legacy anonymous user with key": evaluationcontext.NewAnonymousUser("go-user-7"),
-		"legacy survey":                  evaluationcontext.NewSurvey("go-survey-8"),
-		"legacy survey with account":     evaluationcontext.NewSurvey("go-survey-9", evaluationcontext.WithSurveyAccountID("go-account-9")),
 	}
 
 	err := flags.Configure()
