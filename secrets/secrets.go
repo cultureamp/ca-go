317fba9e7bf3a6b18dfefe7daa8c281e3523397d
Michael RIdgway <michael.ridgway@cultureamp.com>
2024-01-30 10:43:54 +1100

    added comments to public methods and types

--- secrets/secrets.go
+++ secrets/secrets.go
@@ -7,22 +7,26 @@ import (
 	"github.com/aws/aws-sdk-go/service/secretsmanager"
 )
 
-// AWSSecretsManager supports the GetSecretValue method.
+// AWSSecretsManager supports wraps the AWSSecretsManagerClient interface.
 type AWSSecretsManager struct {
 	Client AWSSecretsManagerClient
 }
 
+// NewAWSSecretsManager creates a new AWS Secret Manager for a given region.
 func NewAWSSecretsManager(region string) *AWSSecretsManager {
 	client := newSecretManagerClient(region)
 	return NewAWSSecretsManagerWithClient(client)
 }
 
+// NewAWSSecretsManagerWithClient creates a new AWS Secret Manager with a custom client
+// that supports the AWSSecretsManagerClient interface.
 func NewAWSSecretsManagerWithClient(client AWSSecretsManagerClient) *AWSSecretsManager {
 	return &AWSSecretsManager{
 		Client: client,
 	}
 }
 
+// Get retrives the secret from AWS SecretsManager.
 func (s *AWSSecretsManager) Get(secretName string) (string, error) {
 	input := &secretsmanager.GetSecretValueInput{
 		SecretId: aws.String(secretName),
