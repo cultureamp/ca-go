d71846ad8569f2d8680a904ba63677b6ce33934f
andrew.mccrae <andrew.mccrae@cultureamp.com>
2024-01-30 12:30:00 +1100

    feature add examples with output comment to check test and mock secret results

--- secrets/secrets_example_test.go
+++ secrets/secrets_example_test.go
@@ -6,26 +6,27 @@ import (
 )
 
 func Example() {
-	// this will automatically use the AWS Region as per the environment variable "AWS_REGION"
-	answer, err := secrets.Get("my-test-secret")
-	fmt.Printf("The answer to the secret is '%s' (err='%v')\n", answer, err)
+
+	answer, _ := secrets.Get("my-test-secret")
+	fmt.Printf("test secret is '%s'\n", answer)
 
 	// or if you need secrets from another region other than the one you are running in use
 	//s := secrets.NewAWSSecrets("a-different-region")
-	//answer, err = s.Get("my-test-secret2")
-	//fmt.Printf("The answer to the secret2 is '%s' (err='%v')\n", answer, err)
+	//answer, _ = s.Get("my-test-secret2")
+	//fmt.Printf("The answer to my secret from a different region is '%s'\n", answer)
 	ms := new(mockSecrets)
 	secrets.SetImpl(ms)
-	answer, err = secrets.Get("my-test-secret2")
-	fmt.Printf("The answer to the secret2 is '%s' (err='%v')\n", answer, err)
+	answer, _ = secrets.Get("my-test-secret2")
+	fmt.Printf("mocked secret is '%s' \n", answer)
 
 	//Output:
-	// The answer to the secret is test-secret-value
+	// test secret is 'test-secret-value'
+	// mocked secret is 'mock-secret-value'
 }
 
 type mockSecrets struct {
 }
 
 func (*mockSecrets) Get(key string) (string, error) {
-	return "my-mock-secret", nil
+	return "mock-secret-value", nil
 }
