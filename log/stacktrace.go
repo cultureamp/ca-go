71d3c1f299b1170c21a42291219b797fe7efdc9a
Michael RIdgway <michael.ridgway@cultureamp.com>
2024-03-27 10:27:34 +1100

    removed stack trace string cleaning as its handled for us by the json encoder inside zerolog

--- log/stacktrace.go
+++ log/stacktrace.go
@@ -3,7 +3,6 @@ package log
 import (
 	"bytes"
 	"runtime"
-	"strings"
 
 	"github.com/go-errors/errors"
 )
@@ -17,12 +16,11 @@ func stackTracer(err error) string {
 	// is it the "github.com/go-errors/errors" error type? If so, it has a stack trace we can use
 	var e *errors.Error
 	if errors.As(err, &e) {
-		s := string(e.Stack())
-		return cleanStackTrace(s)
+		return string(e.Stack())
 	}
 
 	// Otherwise we just get the current stack trace (minus the ca-go calls)
-	return cleanStackTrace(currentStack(5))
+	return currentStack(5)
 }
 
 func currentStack(skip int) string {
@@ -37,15 +35,4 @@ func currentStack(skip int) string {
 	}
 
 	return buf.String()
-}
-
-func cleanStackTrace(stack string) string {
-	// since we log in JSON make sure that the stack trace does NOT have any "{", "}" or "\""
-	nr := strings.NewReplacer(
-		"{", "",
-		"}", "",
-		"\"", "",
-	)
-
-	return nr.Replace(stack)
 }
