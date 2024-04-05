29d6c775e2933042e65a22256fe7e3a670e14af5
Michael RIdgway <michael.ridgway@cultureamp.com>
2024-04-05 15:51:26 +1100

    moved autoLogger to experimental

--- kafka/consumer/logger.go
+++ kafka/consumer/logger.go
@@ -2,10 +2,6 @@ package consumer
 
 import (
 	"context"
-	"fmt"
-
-	"github.com/cultureamp/ca-go/log"
-	"github.com/go-errors/errors"
 )
 
 // NotifyError is a notify-on-error function used to report consumer handler errors.
@@ -15,27 +11,4 @@ type ClientLogger interface {
 	// Kafka-go Logger interface
 	Infof(string, ...interface{})
 	Errorf(string, ...interface{})
-}
-
-type autoKafkaLogger struct{}
-
-func (l *autoKafkaLogger) Infof(msg string, args ...interface{}) {
-	details := fmt.Sprintf(msg, args...)
-	log.Info("kafka_reader_info").Details(details)
-}
-
-func (l *autoKafkaLogger) Errorf(msg string, args ...interface{}) {
-	details := fmt.Sprintf(msg, args...)
-	err := errors.Errorf("kafka_reader_error")
-	log.Error("kafka_reader_error", err).Details(details)
-}
-
-func autoClientNotifyError(_ context.Context, err error, msg Message) {
-	log.Error("auto_consumer_notify_error", err).
-		WithSystemTracing().
-		Properties(log.Add().
-			Str("topic", msg.Topic).
-			Str("key", string(msg.Key)).
-			Str("value", string(msg.Value)),
-		).Details("error consuming message")
 }
